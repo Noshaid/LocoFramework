cf4c35bf1e4793d16575ec185c941d63d35d0fa7
Noshaid Ali <noshaid.ali@cricingif.com>
2019-04-26 17:49:26 +0500

    finished our local framework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Noshaid Ali on 26/04/2019.
+//  Copyright © 2019 Cricingif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Noshaid Ali on 26/04/2019.
+//  Copyright © 2019 Cricingif. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+    }
+    
+    public static func doSomething() -> String {
+        return "My first framework!"
+    }
+}
